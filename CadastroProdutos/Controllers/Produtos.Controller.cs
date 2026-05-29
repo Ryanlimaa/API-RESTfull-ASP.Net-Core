798be9e48b26365ee5f77dd8ddadf4176d1ff029
ryan.limaa@outlook.com <ryan.limaa@outlook.com>
2026-05-29 16:02:54 -0300

    Criando o metodo no controller de listar todos os produtos

--- CadastroProdutos/Controllers/Produtos.Controller.cs
+++ CadastroProdutos/Controllers/Produtos.Controller.cs
@@ -7,6 +7,16 @@ namespace CadastroProdutos.controllers
     [ApiController]
     public class ProdutosController : ControllerBase
     {
-        
+        private List<Produto> produtos = new List<Produto>()
+        {
+            new Produto() {Id = 1, Nome = "Mouse sem Fio", Preco = 99.90, Estoque = 50},
+            new Produto() {Id = 2, Nome = "Telcado", Preco = 249.90, Estoque = 30}
+        };
+
+        [HttpGet]
+        public ActionResult<List<Produto>> Get()
+        {
+            return Ok(produtos);
+        }
     }
 }
