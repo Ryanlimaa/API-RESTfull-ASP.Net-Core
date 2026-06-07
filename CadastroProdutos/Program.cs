69d8cfbfa1f0f4edaa183898096674071602e664
ryan.limaa@outlook.com <ryan.limaa@outlook.com>
2026-06-07 15:08:24 -0300

    Validação de dados com DataAnnotations

--- CadastroProdutos/Program.cs
+++ CadastroProdutos/Program.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using CadastroProdutos.Database;
 using CadastroProdutos.Services;
 using Microsoft.EntityFrameworkCore;
@@ -134,7 +135,14 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 public class Produto
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório!")]
+    [StringLength(60, MinimumLength = 3, ErrorMessage = "O nome deve conter entre 3 e 60 caracteres!")]
     public string Nome { get; set; }
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser maior que zero!")]
     public double Preco { get; set; }
+
+    [Range(0, int.MaxValue, ErrorMessage = "O estoque não pode ser negativo!")]
     public int Estoque { get; set; }
 }
