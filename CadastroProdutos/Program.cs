0bd1453d783649dee55b4a3c14ec19478ed241d6
ryan.limaa@outlook.com <ryan.limaa@outlook.com>
2026-06-12 09:26:25 -0300

    Configurando o Swagger para aceitar tokens JWT

--- CadastroProdutos/Program.cs
+++ CadastroProdutos/Program.cs
@@ -18,7 +18,36 @@ builder.Services.AddControllers();
 
 // Adicionando métodos para adicionar Endpoints depois de instalar o Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+    {
+        Description = @"Insira o JWT no campo a baixo seguindo o seguinte formato: Bearer {seu_token}.",
+        Name = "Authorization",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    x.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement()
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = Microsoft.OpenApi.Models.ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 // Registrando a interface para injeção de dependência
 // builder.Services.AddScoped<IProdutosService, ProdutosService>();
